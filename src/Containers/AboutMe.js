6dea79083ac0c6ea13ace2e5de494c49f08f83e9
Leonel Menezes <mml_leo@hotmail.com>
2018-08-03 01:05:11 -0300

    added space after age

--- src/Containers/AboutMe.js
+++ src/Containers/AboutMe.js
@@ -20,8 +20,7 @@ class AboutMe extends React.Component {
                 <div className="aboutme__intro">
                     <div className="aboutme__intro__photo"></div>
                     <div className="aboutme__intro__description">
-                        <p>Hi, I’m a computer scientist, I am {this.getAge('1994/07/29')} 
-                            years old, and you are very welcome in here. 
+                        <p>Hi, I’m a computer scientist, I am {this.getAge('1994/07/29')} years old, and you are very welcome in here. 
                             You should feel free to take a look at my jobs, 
                             ask me anything, or hire me :D</p>
                         <p>I graduated at the Catholic University of Brasilia(UCB), and became a student of the Apple Developer Academy. So I’m capable of building apps and games for iOS, macOS and watchOS. 	At the same time I’m at the apple developer academy, I was able to study some crazy javascript stuff, so I’m also able to make some nodeJS and ReactJS apps.
